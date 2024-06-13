365a27c593c4057896f91b100a893234445f1919
Gayaththri <sriigaya3@gmail.com>
2024-06-13 11:10:13 +0530

    Added FormGenerator component with confirmation dialog

---
+++ src/app/form-generator/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+
+type Props = {};
+
+const FormGenerator = (props: Props) => {
+  return (
+    <Dialog>
+      <DialogTrigger>Open</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button variant="link">Create Manually</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog> 
+  );
+};
+
+export default FormGenerator;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
+import FormGenerator from "./form-generator";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Hello!</Button>
+      <FormGenerator />
     </main>
   );
 }
